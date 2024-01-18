f2be40ab32479245a1dab53a1b11451d92457c60
Ruan Portella <ruanmorales29@gmail.com>
2024-01-18 20:53:05 -0300

    Feat: Cria navigation

--- secao_01/starter/src/screens/HomeScreen.js
+++ secao_01/starter/src/screens/HomeScreen.js
@@ -1,8 +1,20 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>tchau!</Text>;
+const HomeScreen = ({navigation}) => {
+  return (
+    <View>
+      <Text style={styles.text}>Essa é a HomeScreen</Text>
+      <Button 
+        title="Go to Components Demo"
+        onPress={() => navigation.navigate("Components")}  
+      />
+      <Button 
+        title="Go to List Demo"
+        onPress={() => navigation.navigate("List")}  
+      />
+    </View>
+  )
 };
 
 const styles = StyleSheet.create({
